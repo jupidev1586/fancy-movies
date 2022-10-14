b243740e493a716dc043da1b3c81143f95376192
jupidev1586 <gianpaolobuono@gmail.com>
2022-10-14 14:42:42 +0200

    feat: css modal+x closing + height cards

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,15 +1,19 @@
 
 import { memo } from 'react';
+import { useDispatch } from "react-redux";
 import styles from './index.module.scss';
 
 
 const Modal = ({ data }) => {
   const { poster_path, original_title, overview } = data;
 
+  const dispatch = useDispatch();
+
   console.log('data ====>', data)
 
   return (
     <div className={ styles.Modal }>
+      <button className={ styles.button } onClick={() => dispatch({ type: 'SET_MODAL_OFF' })}>X</button>
       <img className={ styles.img } src={`${'https://image.tmdb.org/t/p/original'}${poster_path}`} alt={original_title} />
       <h2 className={ styles.header }>{original_title}</h2>
       <p className={ styles.desc }>{overview}</p>
