9210258533816475bee63e7030fb7a8b24d8b2e2
jupidev1586 <gianpaolobuono@gmail.com>
2022-10-14 09:33:51 +0200

    feat: css navbar and on button search

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,26 @@
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import styles from './index.module.scss';
+import styles from "./index.module.scss";
 
 const Navbar = () => {
   return (
-    <div className={ styles.Navbar }>
-       <ul>
+    <div className={styles.Navbar}>
+      <div className={styles.logo}>
+				<img src="/logo.png" alt="" height="40"/>
+			</div>
+      <ul>
         <li>
-          <a href="#series-list">
-            Series 
-          </a>
+          <a href="#series-list">Series</a>
         </li>
         <li>
-          <a href="#movie-list">
-            Movies 
-          </a>
+          <a href="#movie-list">Movies</a>
         </li>
         <li>
-          <a href="#movie-entity">
-            Search Movie 
-          </a>
+          <a href="#movie-entity">Search Movie</a>
         </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
