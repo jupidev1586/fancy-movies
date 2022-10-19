3cbce716b6f5c42745a30a0012f421437aad15ca
jupidev1586 <gianpaolobuono@gmail.com>
2022-10-19 15:23:49 +0200

    fixes

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,7 @@
 
 import { memo } from 'react';
 import { useDispatch } from "react-redux";
+import { IoIosCloseCircle } from "react-icons/io";
 import styles from './index.module.scss';
 
 
@@ -13,7 +14,7 @@ const Modal = ({ data }) => {
 
   return (
     <div className={ styles.Modal }>
-      <button className={ styles.button } onClick={() => dispatch({ type: 'SET_MODAL_OFF' })}>X</button>
+      <button className={ styles.button } onClick={() => dispatch({ type: 'SET_MODAL_OFF' })}><IoIosCloseCircle /></button>
       <img className={ styles.img } src={`${'https://image.tmdb.org/t/p/original'}${poster_path}`} alt={original_title} />
       <h2 className={ styles.header }>{original_title}</h2>
       <p className={ styles.desc }>{overview}</p>
